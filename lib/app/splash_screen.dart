4a74423ad6cc03d1290de5d8bdf971bcb83b3997
maxwell <zhong.maxwell@gmail.com>
2020-02-24 08:16:06 +0800

    添加图片

---
+++ lib/app/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:xigua_read/app/root_scene.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1500));
+    _animation = Tween(begin: 1.0, end: 1.0).animate(_controller);
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => RootScene()),
+            (route) => route == null);
+      }
+    });
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new FadeTransition(
+      opacity: _animation,
+      child: new Image.asset(
+        'img/launch_image.png',
+        scale: 2.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
